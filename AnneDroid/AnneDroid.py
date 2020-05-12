abce46de2414ef468f1e4c08fcacee2747633a9a
Matthias Schwarz <matthias.schw+github@gmail.com>
2020-05-12 14:21:14 +0200

    added wikipedia search

--- AnneDroid/AnneDroid.py
+++ AnneDroid/AnneDroid.py
@@ -8,8 +8,10 @@ load_dotenv()
 TOKEN = os.getenv('ANNEDROID_TOKEN')
 NAMESPACE = os.getenv('ANNEDROID_NAMESPACE')
 KEYWORD_GOOGLE = os.getenv('ANNEDROID_KEYWORD_GOOGLE')
+KEYWORD_WIKIPEDIA = os.getenv('ANNEDROID_KEYWORD_WIKIPEDIA')
 KEYWORD_HELP = os.getenv('ANNEDROID_KEYWORD_HELP')
-ERROR_MESSAGE = "you are the weakest link"
+ERROR_MESSAGE_COMMAND = "you are the weakest link"
+ERROR_MESSAGE = "i am the weakest link, goodbye!"
 
 client = discord.Client()
 
@@ -33,26 +35,41 @@ async def on_message(message):
                 if(tokens[1].lower() == KEYWORD_HELP):
                     reply += KEYWORD_HELP + ":\n\n"
                     reply += KEYWORD_GOOGLE + " " + "<query>" "\t:\t" + "perform google search"
+                    reply += KEYWORD_WIKIPEDIA + " " + "<query>" "\t:\t" + "perform wikipedia search"
                 else:
                     raise CommandError("invalid command: " + message.content)
 
             elif(len(tokens) > 2):
+                # KEYWORD_GOOGLE
                 if(tokens[1].lower() == KEYWORD_GOOGLE):
                     try:
                         from googlesearch import search
                         searchresult = search(query=" ".join(tokens[2:]), start=0, stop=1)
                         reply += next(searchresult)
-                    except ImportError:
-                        raise CommandError("no module named 'googlesearch' found")
+                    except ImportError as ie:
+                        raise ie
                     except StopIteration:
                         raise CommandError("insufficient results")
                         raise
                     except:
-                        raise CommandError("could not handle search")
                         raise
+                # KEYWORD_WIKIPEDIA
+                if(tokens[1].lower() == KEYWORD_WIKIPEDIA):
+                    try:
+                        from wikipedia import search,page
+                        searchresult = search(query=" ".join(tokens[2:]))
+                        #just lookup the first one
+                        if(len(searchresult) > 0):
+                            wikipage = page(searchresult[0])
+                            reply += wikipage.title + ": " + wikipage.url
+
+                    except ImportError:
+                        raise ie
+                    except:
+                        raise
+
                 else:
                     raise CommandError("unknown command: " + message.content)
-
             else:
                 raise CommandError("invalid command: " + message.content)
 
@@ -62,6 +79,8 @@ async def on_message(message):
 
     except CommandError as e:
         #send the error
-        await message.channel.send(ERROR_MESSAGE + ", " + message.author.name +"!" + "\t" + str(type(e)) + ": " + str(e))
+        await message.channel.send(ERROR_MESSAGE_COMMAND + ", " + message.author.name +"!" + "\t" + str(type(e)) + ": " + str(e))
+    except Exception as e:
+        await message.channel.send(ERROR_MESSAGE + "\t" + str(type(e)) + ": " + str(e))
 
 client.run(TOKEN)
