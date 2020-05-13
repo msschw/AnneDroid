d238e128e5f84231e829de40a173c6796c17238c
Matthias Schwarz <matthias.schw+github@gmail.com>
2020-05-13 21:29:35 +0200

    created command class

--- AnneDroid/AnneDroid.py
+++ AnneDroid/AnneDroid.py
@@ -3,15 +3,11 @@ import os
 
 import discord
 from dotenv import load_dotenv
+from Command import Command
 
 load_dotenv()
 TOKEN = os.getenv('ANNEDROID_TOKEN')
-NAMESPACE = os.getenv('ANNEDROID_NAMESPACE')
-KEYWORD_GOOGLE = os.getenv('ANNEDROID_KEYWORD_GOOGLE')
-KEYWORD_MOSCHUSGOOGLE = os.getenv('ANNEDROID_KEYWORD_MOSCHUSGOOGLE')
-KEYWORD_WIKIPEDIA = os.getenv('ANNEDROID_KEYWORD_WIKIPEDIA')
-KEYWORD_YOUTUBE = os.getenv('ANNEDROID_KEYWORD_YOUTUBE')
-KEYWORD_HELP = os.getenv('ANNEDROID_KEYWORD_HELP')
+
 ERROR_MESSAGE_COMMAND = "you are the weakest link"
 ERROR_MESSAGE = "i am the weakest link, goodbye!"
 
@@ -22,89 +18,25 @@ async def on_message(message):
     from Errors import CommandError
     if(message.author == client.user):
         return
-
+    
     try:
-        # command handling
-        if(message.content.startswith(NAMESPACE)):
-            tokens = message.content.split(' ')
-            reply = ""
-            if(len(tokens) == 2):
-                
-                if(tokens[1].lower() == KEYWORD_HELP):
-                    reply += KEYWORD_HELP + ":\n\n"
-                    reply += KEYWORD_GOOGLE + " " + "<query>" "\t:\t" + "perform google search"
-                    reply += KEYWORD_WIKIPEDIA + " " + "<query>" "\t:\t" + "perform wikipedia search"
-                    reply += KEYWORD_YOUTUBE + " " + "<query>" "\t:\t" + "perform youtube search"
-                else:
-                    raise CommandError("invalid command: " + message.content)
+        if(message.content.startswith(Command.KEYWORD_ACTIVE)):
+            cmd = Command(message.content)
+            reply = cmd.Execute()
 
-            elif(len(tokens) > 2):
-                # KEYWORD_GOOGLE
-                if(tokens[1].lower() == KEYWORD_GOOGLE):
-                    try:
-                        from googlesearch import search
-                        searchresult = search(query=" ".join(tokens[2:]), start=0, stop=1)
-                        reply += next(searchresult)
-                    except ImportError as ie:
-                        raise ie
-                    except StopIteration:
-                        raise CommandError("insufficient results")
-                        raise
-                    except:
-                        raise
-                # KEYWORD_MOSCHUSGOOGLE
-                elif(tokens[1].lower() == KEYWORD_MOSCHUSGOOGLE):
-                    try:
-                        reply += "!google " + " ".join(tokens[2:])
-                    except ImportError as ie:
-                        raise ie
-                    except StopIteration:
-                        raise CommandError("insufficient results")
-                        raise
-                    except:
-                        raise
 
-                # KEYWORD_WIKIPEDIA
-                elif(tokens[1].lower() == KEYWORD_WIKIPEDIA):
-                    try:
-                        from wikipedia import search,page
-                        searchresult = search(query=" ".join(tokens[2:]))
-                        #just lookup the first one
-                        if(len(searchresult) > 0):
-                            wikipage = page(searchresult[0])
-                            reply += wikipage.title + ": " + wikipage.url
-
-                    except ImportError:
-                        raise ie
-                    except:
-                        raise
-                # KEYWORD_YOUTUBE
-                elif(tokens[1].lower() == KEYWORD_YOUTUBE):
-                    try:
-                        from youtube_search import YoutubeSearch
-                        searchresult = YoutubeSearch(search_terms=" ".join(tokens[2:]), max_results=1)
-                        #just lookup the first one
-                        if(len(searchresult.videos) > 0):
-                            reply += searchresult.videos[0]["title"] + ": " + "https://youtube.com" + searchresult.videos[0]["link"]
-
-                    except ImportError:
-                        raise ie
-                    except:
-                        raise
-                else:
-                    raise CommandError("unknown command: " + message.content)
-            else:
-                raise CommandError("invalid command: " + message.content)
-
-            #send the reply
-            if(len(reply) > 0):
-                await message.channel.send(reply)
+        #send the reply
+        if(len(reply) > 0):
+            await message.channel.send(reply)
 
     except CommandError as e:
         #send the error
         await message.channel.send(ERROR_MESSAGE_COMMAND + ", " + message.author.name +"!" + "\t" + str(type(e)) + ": " + str(e))
     except Exception as e:
-        #send internal error
-        await message.channel.send(ERROR_MESSAGE + "\t" + str(type(e)) + ": " + str(e))
+        #do not send internal error
+        #await message.channel.send(ERROR_MESSAGE + "\t" + str(type(e)) + ": " + str(e))
+        #print instead
+        print(ERROR_MESSAGE + "\t" + str(type(e)) + ": " + str(e))
+        pass
 
 client.run(TOKEN)
---
+++ AnneDroid/Command.py
@@ -0,0 +1,157 @@
+from Errors import CommandError
+from enum import Enum
+
+class ActionType(Enum):
+    none = 0 
+    oneshot = 1
+    restart = 2
+    start = 3
+    stop = 4
+    toggle = 5
+
+class CommandType(Enum):
+    google = 1
+    help = 0
+    moogle = 2
+    trivia = 3
+    wiki = 4
+    yt = 5
+
+class Command(object):
+    """command abstraction"""
+
+    KEYWORD_ACTIVE="!ad"
+
+    KEYWORD_COMMAND_GOOGLE="google"
+    KEYWORD_COMMAND_HELP="help"
+    KEYWORD_COMMAND_MOSCHUSGOOGLE="moogle"
+    KEYWORD_COMMAND_WIKIPEDIA="wiki"
+    KEYWORD_COMMAND_YOUTUBE="yt"
+
+
+    KEYWORD_ACTION_ONESHOT="oneshot"
+    KEYWORD_ACTION_RESTART="restart"
+    KEYWORD_ACTION_START="start"
+    KEYWORD_ACTION_STOP="stop"
+    KEYWORD_ACTION_TOGGLE="toggle"
+
+    ActionAssignment = {
+        KEYWORD_ACTION_ONESHOT: ActionType.oneshot,
+        KEYWORD_ACTION_RESTART: ActionType.restart,
+        KEYWORD_ACTION_START: ActionType.start,
+        KEYWORD_ACTION_STOP: ActionType.stop,
+        KEYWORD_ACTION_TOGGLE: ActionType.toggle
+    }
+
+    CommandAssignment = {
+        KEYWORD_COMMAND_GOOGLE: CommandType.google,
+        KEYWORD_COMMAND_HELP: CommandType.help,
+        KEYWORD_COMMAND_MOSCHUSGOOGLE: CommandType.moogle,
+        KEYWORD_COMMAND_WIKIPEDIA: CommandType.wiki,
+        KEYWORD_COMMAND_YOUTUBE: CommandType.yt
+    }
+
+    Action = ActionType.none
+    Command = CommandType.help
+    Query = ""
+
+    def __init__(self, input):
+        self.Parse(input)
+
+    def Parse(self, input):
+        if(not isinstance(input, str)):
+            raise CommandError
+
+        if(not input.startswith(self.KEYWORD_ACTIVE)):
+            self.Action = ActionType.none
+            return
+
+        tokens = input.split(' ')
+
+        # default assignment
+        self.Command = CommandType.help
+        self.Action = ActionType.none
+
+
+        # parse first token
+        if(len(tokens) > 1):
+            if(tokens[1] in self.CommandAssignment):
+                self.Command = self.CommandAssignment[tokens[1]]
+            else:
+                raise CommandError("invalid command: " + tokens[1])
+        
+        # try parse second token
+        firstQueryTokenPosition = 1
+        if(len(tokens) > 2):
+            if(tokens[2] in self.ActionAssignment):
+                # action is specified
+                self.Action = self.ActionAssignment[tokens[2]]
+                firstQueryTokenPosition = 3
+            else:
+                # action unspecified, assume oneshot
+                self.Action = ActionType.oneshot
+                firstQueryTokenPosition = 2
+
+        # try parse query
+        if(len(tokens) > firstQueryTokenPosition):
+            self.Query = " ".join(tokens[firstQueryTokenPosition:])
+
+
+    def Execute(self):
+        if(self.Action == ActionType.none):
+            return ""
+
+        if(self.Command == CommandType.help):
+            reply = self.KEYWORD_HELP + ":\n\n"
+            reply += self.KEYWORD_GOOGLE + " " + "<query>" "\t:\t" + "perform google search"
+            reply += self.KEYWORD_WIKIPEDIA + " " + "<query>" "\t:\t" + "perform wikipedia search"
+            reply += self.KEYWORD_YOUTUBE + " " + "<query>" "\t:\t" + "perform youtube search"
+            return reply
+
+        elif((self.Command == CommandType.google) and (self.Query != "")):
+            try:
+                from googlesearch import search
+                searchresult = search(query=self.Query, start=0, stop=1)
+                return next(searchresult)
+            except ImportError as ie:
+                raise ie
+            except StopIteration:
+                raise CommandError("insufficient results")
+                raise
+            except:
+                raise
+
+        elif((self.Command == CommandType.moogle) and (self.Query != "")):
+            return "!google " + self.Query
+
+        elif((self.Command == CommandType.moogle) and (self.Query != "")):
+            try:
+                from wikipedia import search,page
+                searchresult = search(query=self.Query)
+                #just lookup the first one
+                if(len(searchresult) > 0):
+                    wikipage = page(searchresult[0])
+                    return wikipage.title + ": " + wikipage.url
+                else:
+                    raise CommandError("insufficient results")
+            except ImportError:
+                raise ie
+            except:
+                raise
+
+        elif((self.Command == CommandType.moogle) and (self.Query != "")):
+            try:
+                from youtube_search import YoutubeSearch
+                searchresult = YoutubeSearch(search_terms=self.Query, max_results=1)
+                #just lookup the first one
+                if(len(searchresult.videos) > 0):
+                    return searchresult.videos[0]["title"] + ": " + "https://youtube.com" + searchresult.videos[0]["link"]
+                else:
+                    raise CommandError("insufficient results")
+            except ImportError:
+                raise ie
+            except:
+                raise
+
+        else:
+            raise CommandError("invalid command")
